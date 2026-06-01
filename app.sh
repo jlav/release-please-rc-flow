a26ce0c538f62189e362445666b51b831707f7ce
Joe Laverty <jlav@users.noreply.github.com>
2026-06-01 13:17:28 -0400

    feat: add a help tip to the startup output (#4)

--- app.sh
+++ app.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 echo "hello from release-please-rc-flow v$(cat /version.txt)"
 echo "Welcome aboard! This container is running happily."
+echo "Tip: pass --help to see available options."
